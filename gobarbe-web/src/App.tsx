76f25e5ae97a86ecd14f3e713c047dfd9d41b885
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2020-07-25 02:23:27 -0300

    appling global style

--- gobarbe-web/src/App.tsx
+++ gobarbe-web/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import GlobalStyle from './styles/global';
 import './App.css';
 
-const App: React.FC = () => <h1>oba</h1>
+const App: React.FC = () => (
+  <>
+    <h1>oba</h1>
+    <GlobalStyle/>
+  </>
+)
 
 export default App;
