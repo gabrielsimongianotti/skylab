12eb21d1e4f6976120610bfb1e0423c3f0c669d8
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2020-07-27 13:21:23 -0300

    add controller

---
+++ nivel 2/backend/src/service/CreateAppointmentServices.ts
@@ -0,0 +1,40 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+// Importing the repo so we can define the type of the repositories on the construcotr as an AppointmentsRepository
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+ 
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
--- nivel 3/backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ nivel 3/backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
+
 import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppointmentController from '@modules/appointments/infra/controller/AppointmentsController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
-
+const appointmentsCotroller = new AppointmentController();
 const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -16,19 +16,6 @@ appointmentsRouter.use(ensureAuthenticated);
 // });
 
 // Rota POST
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsCotroller.create);
 
 export default appointmentsRouter;
