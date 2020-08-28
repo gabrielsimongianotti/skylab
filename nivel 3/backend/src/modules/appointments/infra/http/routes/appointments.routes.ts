5744cd67c28587b5a0ad8e45231837d56e2e1bb9
gabrielsimongianotti <gabrielgianotti5@gmail.com.br>
2020-08-28 18:20:13 -0300

    import controllers

--- nivel 3/backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ nivel 3/backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,21 +1,12 @@
 import { Router } from 'express';
 
-import { container } from 'tsyringe';
-
-import AppointmentController from '@modules/appointments/infra/controller/AppointmentsController';
+import AppointmentsController from '@modules/appointments/infra/controller/AppointmentsController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
-const appointmentsCotroller = new AppointmentController();
+const appointmentsCotroller = new AppointmentsController();
 const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthenticated);
 
-// Rota GET
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
-
-// Rota POST
 appointmentsRouter.post('/', appointmentsCotroller.create);
 
 export default appointmentsRouter;
