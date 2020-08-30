8e25b8b0cc3f8dd90b15adc6cf24e3395a522bef
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2020-08-30 18:28:33 -0300

    created first test

--- nivel 3/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ nivel 3/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,9 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
-describe('CreateAppointment', () => {
-  it('should be able to createa new appointment', () => {
-    expect(1 + 2).toBe(3);
+describe('CreateAppointment', async () => {
+  it('should be able to createa new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1234567',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1234567');
   });
 
   // it('should not be able to createa two appointment on the same time', () => {
