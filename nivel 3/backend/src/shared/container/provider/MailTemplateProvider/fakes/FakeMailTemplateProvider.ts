3fb6a6ec80606d1b8a8a657095c013abdae50650
unknown <gabrielgianotti5@gmail.com>
2020-09-19 16:14:45 -0300

    alter import

--- nivel 3/backend/src/shared/container/provider/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ nivel 3/backend/src/shared/container/provider/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,4 +1,4 @@
-import IMailTemplateProvider from '@shared/container/provider/MailTemplatePRovider/models/IMailTemplateProvider';
+import IMailTemplateProvider from '@shared/container/provider/MailTemplateProvider/models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
   public async parse(): Promise<string> {
