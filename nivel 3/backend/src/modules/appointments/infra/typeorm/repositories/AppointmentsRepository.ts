5878a0f1400d71f849ad84c9c60846b0f2ea5267
unknown <gabrielgianotti5@gmail.com>
2020-09-27 04:05:33 -0300

    alter chat to char

--- nivel 3/backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ nivel 3/backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -32,7 +32,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFieldName =>
-            `to_chat(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+            `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
         ),
       },
     });
@@ -53,7 +53,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFieldName =>
-            `to_chat(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     });
